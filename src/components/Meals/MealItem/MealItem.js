b97b4690739867dab160e98403c8088e8247db0c
neha89 <kamini.er@gmail.com>
2023-11-20 12:16:30 +0530

    Added a form next to list of items

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = props => {
@@ -8,6 +9,9 @@ return <li className={classes.meal}>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
+        </div>
+        <div>
+            <MealItemForm />
         </div>
 </li>
 };
